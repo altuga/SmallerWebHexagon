a2ece3f628369da10b361c671245cea19d9c8d24
Altuğ Bilgin Altıntaş <altuga@gmail.com>
2025-12-31 16:45:36 +0300

    Update AppMain to use FileRater with test file and add test_file_rater.txt

--- src/main/java/com/example/smallerwebhexagon/AppMain.java
+++ src/main/java/com/example/smallerwebhexagon/AppMain.java
@@ -2,7 +2,7 @@ package com.example.smallerwebhexagon;
 
 public class AppMain {
     public static void main(String[] args) throws Exception {
-        SmallerWebHexagon hex = new SmallerWebHexagon(new InCodeRater());
+        SmallerWebHexagon hex = new SmallerWebHexagon(new FileRater("src/main/resources/test_file_rater.txt"));
         WebAdapter adapter = new WebAdapter(hex);
         adapter.start();
         System.out.println("Server started. GET /{value} e.g. http://localhost:4567/100");
